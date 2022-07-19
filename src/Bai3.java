35050850532183550ff3b2e8a5300bb81bdf1608
TranMinhThang123 <tranminhthang291102@gmail.com>
2022-07-19 22:29:16 +0700

    BTVN

--- src/Bai3.java
+++ src/Bai3.java
@@ -5,8 +5,8 @@ public class Bai3 {
         int n;
         Scanner scan = new Scanner(System.in);
         n = scan.nextInt();
-        int sum = n+1;
-        for(int i=2;i<=Math.sqrt(n);i++){
+        int sum = 0;
+        for(int i=1;i<=Math.sqrt(n);i++){
             if(n%i==0){
                 if(i*i==n){
                     sum+=i;
